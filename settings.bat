9661ecbffa62576b8f1ece66227568b303d3eae4
Harshal Kudale <54770218+HarshalKudale@users.noreply.github.com>
2021-10-22 23:52:32 +0530

    Nothing

--- settings.bat
+++ settings.bat
@@ -6,7 +6,6 @@ adb connect 127.0.0.1:58526
 
 adb shell monkey -p com.android.settings -c android.intent.category.LAUNCHER 1
 
-exit /s
 
 ::by Harshal Kudale
 ::https://github.com/HarshalKudale/EasySideload-WSA/
