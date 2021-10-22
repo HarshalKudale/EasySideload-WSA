2577783aa698c3fd5e9e47290a6e6c73080a88fe
John Horn <n.urked@gmail.com>
2021-10-22 15:50:12 -0700

    golang installer wrapper

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nurked/EasySideload-WSA
+
+go 1.16
+
+require golang.org/x/sys v0.0.0-20211022215931-8e5104632af7
---
+++ installer.go
@@ -0,0 +1,25 @@
+// +build windows
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+
+	fmt.Println("Installing the following APK:")
+	fmt.Println(os.Args[2])
+
+	fmt.Println("Please wait, installation might take a moment.")
+
+	c := exec.Command("cmd", "/C", "installer.bat", os.Args[2])
+
+	if err := c.Run(); err != nil {
+		fmt.Println("Installation did not succeed. Sorry for that. ", err)
+	}
+
+	fmt.Println("Seems like we are all gucci. Press any key to continue.")
+	fmt.Scanln()
+}
