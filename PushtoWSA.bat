4d8fbc95a822ef3f1dfaf4cafb0661dab7a5f327
John Horn <n.urked@gmail.com>
2021-10-22 16:25:20 -0700

    Removing batch file: completely replaced with go

--- PushtoWSA.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd /d %~dp0
-adb connect 127.0.0.1:58526 
-adb push %1 ./storage/emulated/0/Download
-
-
-:: From https://github.com/loboly-19/EasyPush-WSA
-:: Drag and drop file on this bat file to push them to download folder of android subsystem for linux.
--- installer.go
+++ installer.go
@@ -14,10 +14,27 @@ func main() {
 
 	fmt.Println("Please wait, installation might take a moment.")
 
-	c := exec.Command("cmd", "/C", "installer.bat", os.Args[len(os.Args)-1])
+	fmt.Println("Connecting to ADB.")
+
+	//adb connect 127.0.0.1:58526
+	c := exec.Command("adb", "connect", "127.0.0.1:58526")
 
 	if err := c.Run(); err != nil {
-		fmt.Println("Installation did not succeed. Sorry for that. ", err)
+		fmt.Println("Miserably failed to connect to ADB. Ensure you have Widows 11 installed, and WSA enabled. Turn on debug mode. For more information, check https://bit.ly/wsa-win11", err)
+		fmt.Println("Press enter to close this window.")
+		fmt.Scanln()
+		return
+	}
+
+	//adb install %1
+	fmt.Println("Installing the package.")
+	c2 := exec.Command("adb", "install", os.Args[len(os.Args)-1])
+
+	if err := c2.Run(); err != nil {
+		fmt.Println("Was not able to install the pacakge. check https://bit.ly/wsa-win11", err)
+		fmt.Println("Press enter to close this window.")
+		fmt.Scanln()
+		return
 	}
 
 	fmt.Println("Seems like we are all gucci. Press any key to continue.")
