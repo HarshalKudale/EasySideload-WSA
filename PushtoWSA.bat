874930190f4acfac1e3fe80ec020059042b17e06
Harshal Kudale <54770218+HarshalKudale@users.noreply.github.com>
2021-10-28 16:35:35 +0530

    Fixed multiple devices error

--- PushtoWSA.bat
+++ PushtoWSA.bat
@@ -3,7 +3,7 @@ set "package=%1"
 
 if exist %appdata%\adb\ (
 %appdata%\adb\adb connect 127.0.0.1:58526 
-%appdata%\adb\adb push %1 ./storage/emulated/0/Download
+%appdata%\adb\adb -s 127.0.0.1:58526 push %1 ./storage/emulated/0/Download
 EXIT /B
 ) else (
 echo Run fixer.bat to fix error
