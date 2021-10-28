9f1523138e509260d3893167d6d6cd0738b5cdf2
Harshal Kudale <54770218+HarshalKudale@users.noreply.github.com>
2021-10-28 16:33:20 +0530

    Added fix for multiple adb devices found

--- installer.bat
+++ installer.bat
@@ -3,7 +3,7 @@ set "package=%1"
 
 if exist %appdata%\adb\ (
 %appdata%\adb\adb connect 127.0.0.1:58526 
-%appdata%\adb\adb install %package%
+%appdata%\adb\adb -s 127.0.0.1:58526 install %package%
 EXIT /B
 ) else (
 echo Run fixer.bat to fix error
